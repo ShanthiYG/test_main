06a3ab49aac58758a14e28d23c88a403ea803296
Shanthi <Shanthi876@hotmail.com>
2020-10-13 22:20:54 -0400

    Repo

---
+++ src/main/java/Devops1/App.java
@@ -0,0 +1,13 @@
+package Devops1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
